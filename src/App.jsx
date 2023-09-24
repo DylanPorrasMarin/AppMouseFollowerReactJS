a708bc5bcc02ca43f8f95a4d2586686d3e9ee97c
Dylan <porrasmarindylan@gmail.com>
2023-09-24 14:47:08 -0600

    Update code with componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+const FollowMouse = ()=>{
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition]  = useState({x: 0, y: 0})
+  //El useEffect siempre tiene que estar en el cuepro del componente, y no puede declararse dentro de un if o alguna estructua de ciclo
+
+  useEffect(()=>{
+    console.log('effect')
+
+    const handleMove = (event)=>{
+      const {clientX, clientY} = event
+      console.log('handleMove', {clientX, clientY})
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if(enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    //Para poder desactivar el useEffect al darle el boton se tiene que hacer un cleanEffect, y se le indica al hacer el return 
+    return ()=>{
+      //Limpiar elementos y su evento para evitar suscripciones repetidas del evento
+      window.removeEventListener('pointermove', handleMove)
+    }
+
+  },[enabled])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor:'rgba(0,0,0,0.5)',
+        border: '1px solid #fff',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 40,
+        height: 40,
+        borderRadius: '50%',
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Hola</h3>
+      <button onClick={()=>{setEnabled(!enabled)}}>{enabled? 'Desactivar' : 'Activar'} seguir puntero</button>
+    </main>
     </>
+  )
+
+}
+
+function App() {
+
+  return(
+    <main>
+      <FollowMouse/>
+    </main>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+  //El StrictMode renderiza dos veces los componentes en desarrollo para saber si hay errores 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
